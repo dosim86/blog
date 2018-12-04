115d462421e97c9b9f028407840e0db4bee03dc2
dosim86 <misstilda@yandex.ru>
2018-12-05 01:29:00 +0600

    add & edit blog buttons

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@
 namespace App\Controller;
 
 use App\Entity\Blog;
+use App\Form\BlogType;
 use App\Repository\BlogRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,6 +26,49 @@ class BlogController extends AbstractController
 
         return $this->render('blog/index.html.twig', [
             'pagination' => $pagination
+        ]);
+    }
+
+    /**
+     * @Route("blog/add", name="blog_add")
+     */
+    public function add(Request $request)
+    {
+        $blog = new Blog();
+
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blog);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_show', ['slug' => $blog->getSlug()]);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Create new blog'
+        ]);
+    }
+
+    /**
+     * @Route("blog/edit/{id}", name="blog_edit")
+     */
+    public function edit(Blog $blog, Request $request)
+    {
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('blog_show', ['slug' => $blog->getSlug()]);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Update: ' . $blog->getTitle()
         ]);
     }
 
