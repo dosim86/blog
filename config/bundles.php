06015a1c0aa1cc5cab5dd6e501ac65d11a096b2d
dosim86 <misstilda@yandex.ru>
2018-12-05 00:16:04 +0600

    showing blogs

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,19 +5,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
 use App\Repository\BlogRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
+     * @Route("/blog", name="blog_index")
      */
-    public function index(BlogRepository $blogRepository)
+    public function index(Request $request, BlogRepository $blogRepository, PaginatorInterface $paginator)
     {
+        $queryBuilder = $blogRepository->getWithQueryBuilder();
+        $pagination = $paginator->paginate($queryBuilder, $request->query->getInt('page', 1), 10);
+
         return $this->render('blog/index.html.twig', [
-            'blogs' => $blogRepository->findAll()
+            'pagination' => $pagination
+        ]);
+    }
+
+    /**
+     * @Route("blog/{slug}", name="blog_show")
+     */
+    public function show(Blog $blog)
+    {
+        return $this->render('blog/show.html.twig', [
+            'blog' => $blog
         ]);
     }
 }
