5cfa7330438ed41c071d49bcb008108770946427
dosim86 <misstilda@yandex.ru>
2019-01-13 23:52:53 +0500

    added seo functionality

---
+++ src/Entity/Seo.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SeoRepository")
+ */
+class Seo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $keywords;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isDisabled;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getKeywords(): ?string
+    {
+        return $this->keywords;
+    }
+
+    public function setKeywords(?string $keywords): self
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isDisabled(): ?bool
+    {
+        return $this->isDisabled;
+    }
+
+    public function setIsDisabled(bool $disabled): self
+    {
+        $this->isDisabled = $disabled;
+
+        return $this;
+    }
+}
---
+++ src/Twig/SeoExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Twig;
+
+use App\Repository\SeoRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SeoExtension extends AbstractExtension
+{
+    private $title;
+    private $keywords;
+    private $description;
+
+    public function __construct(RequestStack $requestStack, SeoRepository $repository)
+    {
+        $request = $requestStack->getCurrentRequest();
+        $seo = $repository->findOneBy([
+            'isDisabled' => false,
+            'path' => $request->getPathInfo()
+        ]);
+
+        if ($seo) {
+            $this->title = $seo->getTitle() ?: null;
+            $this->keywords = $seo->getKeywords() ?: null;
+            $this->description = $seo->getDescription() ?: null;
+        }
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('seo_title', [$this, 'title'], ['is_safe' => ['html']]),
+            new TwigFunction('seo_keywords', [$this, 'keywords'], ['is_safe' => ['html']]),
+            new TwigFunction('seo_description', [$this, 'description'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function title()
+    {
+        return $this->title ? '<title>' . $this->title . '</title>' : '';
+    }
+
+    public function keywords()
+    {
+        return $this->keywords ? '<meta name="keywords" content="' . $this->keywords . '">' : '';
+    }
+
+    public function description()
+    {
+        return $this->description ? '<meta name="description" content="' . $this->description . '">' : '';
+    }
+}
